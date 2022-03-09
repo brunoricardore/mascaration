6387398a60d9ec527877c56f8e46abfe0cbf18d0
Bruno Ricardo <brunoricardore@gmail.com>
2022-03-09 20:24:05 -0300

    added array of masks feature

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,58 @@
-function formatter(mask: string | string[], value: string, placeholderChar?: string) {
-    if (!value || !mask) return;
-    
-    const splitted_value = value.split('');
-    let finalMask = '';
-  
-    if (Array.isArray(mask)) {
-      let bestMask = mask.map(m => m.replace(/[^#]/g, '').length).sort( (a,b) => Math.abs(a - splitted_value.length) - Math.abs(b - splitted_value.length));
-    } else {
-      finalMask = mask.toString();
-    }
-  
-    let formatted = [];
-  
-    const splitted_mask = finalMask.split('');
-  
-    let skipLength = 0;
-    for (let index = 0; index < splitted_mask.length; index++) {
-        
-      if (splitted_mask[index] !== '#') {
-        formatted[index] = splitted_mask[index];
-        skipLength++;
-        continue;
-      }
-      formatted[index] = splitted_value[index - skipLength] ? splitted_value[index - skipLength] : placeholderChar || splitted_mask[index];
-    }
-  
-    return formatted.join('');
+
+/**
+ * 
+ * @param mask The mask to format the value: '###.###.###-##', if you give an array of masks, it found the best match by the mask size (including only maskChar), and use it to mask
+ * @param value Value you'd like to mask
+ * @param maskChar change the replacer char, instead of '#'
+ * @param placeholderChar you can define an placeholder char to empty slots: '###.###.___-__'
+ * @returns string
+ */
+function formatter(mask: string | string[], value: string, maskChar: string = '#', placeholderChar?: string): string {
+  if (!value || !mask) return '';
+
+  const splitted_value = value.split('');
+  let finalMask = '';
+
+  if (Array.isArray(mask)) {
+    let bestMask = mask
+      .map(m => {
+        const onlyMaskChars = m.replace(new RegExp(`/[^${maskChar}]/g`), '');
+        return {
+          mask: m,
+          length: onlyMaskChars.length
+        }
+      }).sort((a, b) => Math.abs(a.length - splitted_value.length) - Math.abs(b.length - splitted_value.length));
+      finalMask = bestMask[0].mask;
+  } else {
+    finalMask = mask.toString();
   }
 
-  function unmask(maskedValue: string): string {
-    return maskedValue.replace(/[\D]/, '');
+  let formatted = [];
+
+  const splitted_mask = finalMask.split('');
+
+  let skipLength = 0;
+  for (let index = 0; index < splitted_mask.length; index++) {
+
+    if (splitted_mask[index] !== '#') {
+      formatted[index] = splitted_mask[index];
+      skipLength++;
+      continue;
+    }
+    formatted[index] = splitted_value[index - skipLength] ? splitted_value[index - skipLength] : placeholderChar || splitted_mask[index];
   }
 
+  return formatted.join('');
+}
 
-  export {formatter, unmask}
+/**
+ * 
+ * @param maskedValue the masked value ''123.123.123.11'
+ * @returns string 12312312311
+ */
+function unmask(maskedValue: string): string {
+  return maskedValue.replace(/[\D]/, '');
+}
+
+
+export { formatter, unmask }
